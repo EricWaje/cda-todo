96985d7897de788e342971172f2d37c3034586c1
Eric Wajnrajch <ericwaje03@gmail.com>
2022-08-22 14:14:05 -0300

    agregando devTools

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { loginReducer } from './reducers/loginReducer';
 
 export const RootReducers = combineReducers({
     login: loginReducer,
 });
 
-export default createStore(RootReducers);
+export default createStore(RootReducers, composeWithDevTools());
